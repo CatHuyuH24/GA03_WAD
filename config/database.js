66e57835f7a963f368299321768f246101c442f8
Ma Cat Huynh <uynhhuc810@gmail.com>
2024-11-10 19:13:36 +0700

    refactor: delete unnecessary console.log Lines
    Deployed successfully to Render, deleting old console.log lines

--- config/database.js
+++ config/database.js
@@ -3,10 +3,8 @@ require("dotenv").config();
 
 let pool;
 
-if (process.env.NODE_ENV === "PROD") {
-    console.log("Using Production Pool from database.js");
-    console.log("DB_CONNECTION_STRING from database.js: ", process.env.DB_CONNECTION_STRING);
-    
+if (process.env.NODE_ENV === "PROD") 
+{
     // Production Pool
     pool = new Pool({
         connectionString: process.env.DB_CONNECTION_STRING,
@@ -16,9 +14,9 @@ if (process.env.NODE_ENV === "PROD") {
         max: parseInt(process.env.DB_MAX_CLIENTS),
         idleTimeoutMillis: parseInt(process.env.DB_IDLE_TIMEOUT_MILLIS),
     });
-} else {
-    console.log("Using Development Pool from database.js");
-    
+} 
+else 
+{
     // Development Pool
     pool = new Pool({
         host: process.env.DB_HOST,
